d0c2b0d464954aa6760d35f18171f1cb1aead73a
Luongtuan1612 <natsuseth654@gmail.com>
2025-05-09 23:28:03 +0700

    update

---
+++ src/AreaCalculator.java
@@ -0,0 +1,9 @@
+public class AreaCalculator {
+    public double calculateTotalArea(Shape[] shapes) {
+        double total = 0;
+        for (Shape shape : shapes) {
+            total += shape.calculateArea();
+        }
+        return total;
+    }
+}
